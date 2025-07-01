05239f931a3327518d30934d4a3b6a1282bad1f8
devamahayana <devamahayanatop@gmail.com>
2025-07-01 15:23:46 +0800

    Update dashboard logo dan href logo

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,12 +1,12 @@
 <div class="main-nav">
     <!-- Sidebar Logo -->
     <div class="logo-box">
-        <a href="index.html" class="logo-dark">
+        <a href="" class="logo-dark">
             <img src="{{ asset('admin') }}/assets/images/logo-sm.png" class="logo-sm" alt="logo sm">
             <img src="{{ asset('admin') }}/assets/images/logo-dark.png" class="logo-lg" alt="logo dark">
         </a>
 
-        <a href="index.html" class="logo-light">
+        <a href="" class="logo-light">
             <img src="{{ asset('admin') }}/assets/images/logo-sm.png" class="logo-sm" alt="logo sm">
             <img src="{{ asset('admin') }}/assets/images/logo-light.png" class="logo-lg" alt="logo light">
         </a>
